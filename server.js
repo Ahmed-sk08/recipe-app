09081b78bc461739756ab9ddeacff3113c0ec564
Kanabu <kanabuluther@gmail.com>
2024-11-26 19:08:46 +0300

    Added DELETE endpoint

--- server.js
+++ server.js
@@ -37,8 +37,27 @@ app.post('/recipes', (req, res) => {
   });
 });
 
+// Delete recipe
+app.delete('/recipes/:name', (req, res) => {
+  const recipeName = decodeURIComponent(req.params.name);
+  
+  fs.readFile(RECIPES_FILE, (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading recipes file');
+    }
+    let recipes = JSON.parse(data);
+    recipes = recipes.filter(recipe => recipe.name !== recipeName);
+    fs.writeFile(RECIPES_FILE, JSON.stringify(recipes, null, 2), (err) => {
+      if (err) {
+        return res.status(500).send('Error writing recipes file');
+      }
+      res.send('Recipe deleted');
+    });
+  });
+});
+
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
